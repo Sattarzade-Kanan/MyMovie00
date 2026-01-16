43e9efd499ecd47e7de818506dbc9b49739b2d37
Sattarzade-Kanan <chinkan1980@gmail.com>
2026-01-17 01:09:42 +0400

    BLA BLA BLAAA

--- demo/src/main/java/com/example/demo/dto/MovieForm.java
+++ demo/src/main/java/com/example/demo/dto/MovieForm.java
@@ -18,4 +18,43 @@ public class MovieForm {
     @NotBlank(message ="Genre required")
     private String genre;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
 }
